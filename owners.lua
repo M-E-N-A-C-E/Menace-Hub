a759d069e2ec958eb76acffb3e0dd59bd9f691f7
M-E-N-A-C-E <menaceontopfr@gmail.com>
2025-05-28 05:41:01 +0800

    Create owners.lua

---
+++ owners.lua
@@ -0,0 +1,15 @@
+return {
+    "a55af82d82574",
+    "aldrinitono",
+    "7e9acc98db2622a3bd",
+    "SonicTheHeadTop",
+    "f6407d229b707fba0",
+    "Rizzmeupcid",
+    "Ryan12M9x",
+    "w8Xr5bFh2K",
+    "A1pT7jQ0kW",
+    "aB3xY9zK7M",
+    "kuwalski",
+    "K3BZNm7LpA",
+    "sD8N5vKX7Y"
+}
