5a33bb4fdda4bf4130bf38bc0d480c9d09fffe93
M-E-N-A-C-E <menaceontopfr@gmail.com>
2025-05-28 16:51:28 +0800

    Update mods.lua

--- mods.lua
+++ mods.lua
@@ -1,5 +1,10 @@
 return {
     "noob9999999555",
     "yadejen_9",
-    "AIbertmrfilmaa"
+    "AIbertmrfilmaa",
+    "1x1x1x1xfromhell",
+    "leftgamer404",
+    "crossxyy",
+    "Usjdjdn747",
+    "Diego29wi"
 }
